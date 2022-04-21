2bdf8779e18b030a6db0ce6f7e6c87bc7fd8845c
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-20 22:52:14 -0700

    compile error. tsc knows the input is not a number

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,4 +1,4 @@
-function add(n1, n2) {
+function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
@@ -6,4 +6,4 @@ const number1 = "5";
 const number2 = 2.8;
 
 const result = add(number1, number2);
-console.log(result)
+console.log(result);
