72309f4970199c3823e1d53cdcf547aeef0d83ed
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-21 22:55:13 -0700

    revised to calculate before concatinate

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,13 +1,15 @@
-function add(n1: number, n2: number, showResult: boolean) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 
   // if (typeof n1 !== "number" || typeof n2 !== "number") {
   //   throw new Error("incorrect input")
   // }
 
+  let result = n1 + n2;
+
   if (showResult) {
-    console.log(n1 + n2);
+    console.log(phrase + result);
   } else {
-    return n1 + n2;
+    return result;
   }
 
 }
@@ -15,6 +17,7 @@ function add(n1: number, n2: number, showResult: boolean) {
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultPhrase = "Result is: ";
 
 
-add(number1, number2, printResult);
+add(number1, number2, printResult, resultPhrase);
