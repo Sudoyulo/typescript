94c2853a2158bc8cfbb24968ab3f14944c2d83f1
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-24 21:53:25 -0700

    ts detects string array

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,9 +1,16 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "max",
+//   age: 30
+// }
+//dont do this
+
+const person = {
   name: "max",
-  age: 30
+  age: 30,
+  hobbies: ["spots", "cooking"]
 }
 
 console.log(person.name);
