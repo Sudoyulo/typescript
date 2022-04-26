ead901ebc4d8598d99f42f8b603efd146b3c2418
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-25 23:44:59 -0700

    choose starting number

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -25,16 +25,16 @@
 // const READ_ONLY = 1;
 
 enum Role {
-  ADMIN,
-  READ_ONLY,
-  AUTHOR,
-} //enum
+  ADMIN = 5,
+  READ_ONLY, //6
+  AUTHOR, //7
+} //enum 0,1,2
 
 const person = {
   name: "max",
   age: 30,
   hobbies: ["sports", "cooking"],
-  role: 0,
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin"); //ts cant catch push
