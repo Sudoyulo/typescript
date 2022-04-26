ad909b959e601ea7e4201cefc183e676bab5c2c8
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-25 23:26:52 -0700

    my first enum

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -9,16 +9,32 @@
 // }
 //do this for tuples.
 
-const person: {
-  name: "max";
-  age: 30;
-  hobbies: string[];
-  role: [number, string]; //tuple declaration
-} = {
+// const person: {
+//   name: "max";
+//   age: 30;
+//   hobbies: string[];
+//   role: [number, string]; //tuple declaration
+// } = {
+//   name: "max",
+//   age: 30,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"], //tuple
+// };
+
+// const ADMIN = 0; //normal way
+// const READ_ONLY = 1;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+} //enum
+
+const person = {
   name: "max",
   age: 30,
   hobbies: ["sports", "cooking"],
-  role: [2, "author"], //tuple
+  role: 0,
 };
 
 // person.role.push("admin"); //ts cant catch push
