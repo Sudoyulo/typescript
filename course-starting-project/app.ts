43b315e7f623d58ba3a86b24b436be68e75b65b8
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-28 21:21:38 -0700

    created a callback function

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -7,6 +7,11 @@ function printResult(num: number) {
   console.log("Result:" + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combinedValues: (a: number, b: number) => number;
@@ -15,3 +20,7 @@ combinedValues = add; //stored a pointer to a function
 // combinedValues = 5;
 
 console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
