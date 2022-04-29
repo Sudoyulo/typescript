ed269c5947fd714036ac6a205476d7ca65296417
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-28 21:14:15 -0700

    stored a function as a value

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -2,10 +2,15 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-function printResult(num: number): undefined {
+function printResult(num: number) {
   //void return type
   console.log("Result:" + num);
-  return;
 }
 
 printResult(add(5, 12));
+
+let combinedValues;
+
+combinedValues = add; //stored a pointer to a function
+
+console.log(combinedValues(8, 8));
