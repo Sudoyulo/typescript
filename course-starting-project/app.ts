68d0219c99420770bb84418dd714592a4b323f9d
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-28 21:18:20 -0700

    gave function types specific to type acceptable

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -9,9 +9,9 @@ function printResult(num: number) {
 
 printResult(add(5, 12));
 
-let combinedValues: Function;
-
+let combinedValues: (a: number, b: number) => number;
+//accepts any function that takes 2 number paramaters that return a number
 combinedValues = add; //stored a pointer to a function
-combinedValues = 5;
+// combinedValues = 5;
 
 console.log(combinedValues(8, 8));
