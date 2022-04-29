c13219367a12310c0e67989feeaa54250b2c95b8
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-28 21:11:49 -0700

    undefined is a type but void is better when returning nothing

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -2,9 +2,10 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-function printResult(num: number) {
+function printResult(num: number): undefined {
   //void return type
   console.log("Result:" + num);
+  return;
 }
 
 printResult(add(5, 12));
