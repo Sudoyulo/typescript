768889a8a9fde634c4a8ea16a1ef93419d167011
Kevin Tan Yulo <yulokevin@gmail.com>
2022-05-04 21:21:33 -0700

    scope works because of var but not if let. test in Chrome console to see true output

--- s4-next-generation/dist/app.js
+++ s4-next-generation/dist/app.js
@@ -2,4 +2,9 @@
 const userName = "kev";
 let age = 31;
 age = 32;
+function add(a, b) {
+    var result;
+    result = a + b;
+    return result;
+}
 //# sourceMappingURL=app.js.map
--- s4-next-generation/src/app.ts
+++ s4-next-generation/src/app.ts
@@ -1,7 +1,18 @@
 // Code goes here!
 
 const userName = "kev";
-userName = "max";
+
 let age = 31;
 
 age = 32;
+
+function add(a: number, b: number) {
+  var result;
+  result = a + b;
+  return result;
+}
+
+if (age > 20) {
+  var isOld = true;
+}
+console.log(isOld);
